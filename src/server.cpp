56db37617f00b74ddea18a40d91f0a330fdf53b5
Guilloux Nathan <guillouxnathan22@gmail.com>
2024-07-14 21:59:30 +0200

    send ok response

--- src/server.cpp
+++ src/server.cpp
@@ -53,10 +53,17 @@ int main(int argc, char **argv)
 
   std::cout << "Waiting for a client to connect...\n";
 
-  accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
+  int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len);
   std::cout << "Client connected\n";
 
+  std::string response_ok = "HTTP/1.1 200 OK\r\n\r\n";
+
+  if (send(client_fd, response_ok.c_str(), response_ok.size(), 0) == -1)
+  {
+    std::cout << "Couldn't send response\n";
+  }
+
   close(server_fd);
- 
+
   return 0;
 }
